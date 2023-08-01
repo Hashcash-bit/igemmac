a1542db28a6d48fec23bedff33d75bda971cfa1e
Hashcash-bit <hashirimam2004@gmail.com>
2023-08-01 18:20:39 -0400

    Update: Slider (Complete)

--- src/Pages/Components/InfoSlider/SliderStyles.js
+++ src/Pages/Components/InfoSlider/SliderStyles.js
@@ -1,5 +1,30 @@
+import styled, { keyframes } from "styled-components";
 
-import styled, { keyframes } from 'styled-components';
+export const Container = styled.div`
+  font-family: Montserrat-Alt1;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  cursor: default;
+`;
+
+export const Box = styled.div`
+  height: max-content;
+  background-color: #96080a;
+  display: flex;
+  height: 75px;
+  width: 98%;
+  border-radius: 50px;
+  margin-top: 15px;
+  justify-content: center;
+  align-items: center;
+  border: 4px solid white;
+  overflow: hidden;
+
+  @media screen and (max-width: 500px) {
+    padding-right: 0px;
+  }
+`;
 
 export const slideAnimation = keyframes`
   0% {
@@ -18,8 +43,9 @@ export const SliderContainer = styled.div`
 
 export const SlideWrapper = styled.div`
   display: inline-flex;
-  animation: ${slideAnimation} 150s linear infinite;
+  animation: ${slideAnimation} 30s linear infinite;
   white-space: nowrap;
+  width: 90%;
 
   /* Add additional styles to make the sliding smooth */
   &:hover {
@@ -30,10 +56,22 @@ export const SlideWrapper = styled.div`
 export const SlideItem = styled.span`
   margin-right: 30px;
   font-size: 24px;
-  color: #333;
+  color: #daa9a9;
+  font-weight: bold;
 `;
 
-export const StaticText = styled.span`
+export const StaticText = styled.div`
   font-size: 24px;
-  color: #333;
+  font-weight: bold;
+  color: white;
+  background-color: #96080a;
+  z-index: 5;
+  height: 75px;
+  width: 10%;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  border-top: 5px solid white;
+  border-right: 5px solid white;
+  border-bottom: 5px solid white;
 `;
