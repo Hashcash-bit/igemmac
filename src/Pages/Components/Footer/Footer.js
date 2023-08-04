6cf44e6ebff31147634f8584f57a99a7b7280e16
Hashcash-bit <hashirimam2004@gmail.com>
2023-08-04 17:03:55 -0400

    Update: Footer (Styling Now + Organized)

--- src/Pages/Components/Footer/Footer.js
+++ src/Pages/Components/Footer/Footer.js
@@ -1,113 +1,10 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 
-const FooterWrapper = styled.footer`
-  background-color: #333;
-  color: #fff;
-  padding: 30px;
-`;
-
-const FooterContent = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
-  gap: 20px;
-  text-align: center;
-
-  @media (min-width: 768px) {
-    grid-template-columns: repeat(3, 1fr);
-    text-align: left;
-  }
-`;
-
-const LogoSection = styled.div`
-  @media (min-width: 768px) {
-    grid-column: span 1;
-  }
-`;
-
-const Logo = styled.img`
-  width: 80px;
-`;
-
-const Description = styled.p`
-  margin-top: 10px;
-`;
-
-const LinksSection = styled.div`
-  @media (min-width: 768px) {
-    grid-column: span 2;
-  }
-`;
-
-const LinksTitle = styled.h3`
-  color: #fff;
-  font-size: 18px;
-  font-weight: bold;
-`;
-
-const LinksList = styled.ul`
-  list-style: none;
-  padding: 0;
-  margin: 0;
-`;
-
-const LinkItem = styled.li`
-  margin-bottom: 8px;
-`;
-
-const Link = styled.a`
-  color: #fff;
-  text-decoration: none;
-  transition: opacity 0.2s;
-
-  &:hover {
-    opacity: 0.8;
-  }
-`;
-
-const SubscribeWrapper = styled.div`
-  @media (min-width: 768px) {
-    grid-column: span 3;
-    display: flex;
-    justify-content: center;
-  }
-`;
-
-const SubscribeInput = styled.input`
-  padding: 10px;
-  margin-right: 10px;
-  border: 1px solid #fff;
-  border-radius: 5px;
-  color: #333;
-  background-color: #fff;
-`;
-
-const SubscribeButton = styled.button`
-  padding: 10px 20px;
-  border: none;
-  border-radius: 5px;
-  background-color: #f95d9b;
-  color: #fff;
-  cursor: pointer;
-  transition: background-color 0.2s;
-
-  &:hover {
-    background-color: #e24c89;
-  }
-`;
-
-const FooterBox = styled.div`
-  background-color: #444;
-  padding: 20px;
-  text-align: center;
-`;
-
-const CopyRightText = styled.p`
-  margin-top: 20px;
-  text-align: center;
-  font-size: 14px;
-  color: #fff;
-`;
+//Logo Import
+import Logo from "../img/Logo.svg";
+import Heart from "../img/Heart.svg";
+import { ActualLogo, FooterBox, FooterContainer, HeartLogo, LeftArea, LeftSB, LikkleDes, LikkleTitle, LinkBox, LinkHeader, LogoContainer, RightArea, RightSB, SubButton, SubLinkBox, SubsBox } from "./FooterStyles";
+import { Links } from "../TopSection/NavbarStyled";
 
 const Footer = () => {
   const [email, setEmail] = useState("");
@@ -123,73 +20,72 @@ const Footer = () => {
   };
 
   return (
-    <FooterWrapper>
-      <FooterContent>
-        <LogoSection>
-          <Logo src="/path/to/logo.png" alt="Your Brand" />
-          <Description>
-            Your Brand's tagline or description goes here.
-          </Description>
-        </LogoSection>
-        <LinksSection>
-          <LinksTitle>Quick Links</LinksTitle>
-          <LinksList>
-            <LinkItem>
-              <Link href="#">Home</Link>
-            </LinkItem>
-            <LinkItem>
-              <Link href="#">About</Link>
-            </LinkItem>
-            <LinkItem>
-              <Link href="#">Services</Link>
-            </LinkItem>
-            <LinkItem>
-              <Link href="#">Contact</Link>
-            </LinkItem>
-            <LinkItem>
-              <Link href="#">Blog</Link>
-            </LinkItem>
-            {/* Adding demo links to showcase the column layout */}
-            <LinkItem>
-              <Link href="#">Link 1</Link>
-            </LinkItem>
-            <LinkItem>
-              <Link href="#">Link 2</Link>
-            </LinkItem>
-            <LinkItem>
-              <Link href="#">Link 3</Link>
-            </LinkItem>
-            <LinkItem>
-              <Link href="#">Link 4</Link>
-            </LinkItem>
-            <LinkItem>
-              <Link href="#">Link 5</Link>
-            </LinkItem>
-          </LinksList>
-        </LinksSection>
-        <SubscribeWrapper>
-          <SubscribeInput
-            type="email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={handleEmailChange}
-          />
-          <SubscribeButton onClick={handleSubscribe}>Subscribe</SubscribeButton>
-        </SubscribeWrapper>
-      </FooterContent>
-      <FooterBox>
-        {/* Adding some content below the subscribe section */}
-        <p>Follow us on social media</p>
-        <div>
-          <Link href="#">Facebook</Link>
-          <Link href="#">Twitter</Link>
-          <Link href="#">Instagram</Link>
-        </div>
-      </FooterBox>
-      <CopyRightText>
-        &copy; {new Date().getFullYear()} Your Brand. All rights reserved.
-      </CopyRightText>
-    </FooterWrapper>
+    <>
+      <SubsBox>
+        <LeftSB>
+          <LikkleTitle></LikkleTitle>
+          <LikkleDes></LikkleDes>
+        </LeftSB>
+        <RightSB>
+          <input />
+          <SubButton>Subscribe</SubButton>
+        </RightSB>
+      </SubsBox>
+      <FooterContainer>
+        <FooterBox>
+          <LeftArea>
+            <LogoContainer>
+              <HeartLogo src={Heart} />
+              <ActualLogo src={Logo} />
+            </LogoContainer>
+          </LeftArea>
+          <RightArea>
+            <LinkBox>
+              <LinkHeader>ABOUT</LinkHeader>
+              <SubLinkBox>
+                <Links>Our Team</Links>
+                <Links>Our Mission</Links>
+                <Links>Join Us</Links>
+              </SubLinkBox>
+            </LinkBox>
+            <LinkBox>
+              <LinkHeader>INITIATIVES</LinkHeader>
+              <SubLinkBox>
+                <Links>STEMcast</Links>
+                <Links>Conference</Links>
+                <Links>Biolab Tutorials</Links>
+                <Links>McMaster Course</Links>
+              </SubLinkBox>
+            </LinkBox>
+            <LinkBox>
+              <LinkHeader>PROJECTS</LinkHeader>
+              <SubLinkBox>
+                <Links>Current</Links>
+                <Links>2022</Links>
+                <Links>2021</Links>
+                <Links>2020</Links>
+              </SubLinkBox>
+            </LinkBox>
+            <LinkBox>
+              <LinkHeader>NEWS</LinkHeader>
+              <SubLinkBox>
+                <Links>Bio News</Links>
+                <Links>Genetic News</Links>
+                <Links>Franky Fucks</Links>
+              </SubLinkBox>
+            </LinkBox>
+            <LinkBox>
+              <LinkHeader>PARTNERS</LinkHeader>
+              <SubLinkBox>
+                <Links>Sponsors</Links>
+                <Links>Collaborators</Links>
+                <Links>Partner With Us</Links>
+              </SubLinkBox>
+            </LinkBox>
+          </RightArea>
+        </FooterBox>
+      </FooterContainer>
+    </>
   );
 };
 
---
+++ src/Pages/Components/Footer/FooterStyles.js
@@ -0,0 +1,23 @@
+//Styled Components
+import styled from "styled-components";
+
+//Routing
+import { Link } from "react-router-dom";
+
+export const SubsBox = styled.div``;
+export const LeftSB = styled.div``;
+export const LikkleTitle = styled.div``;
+export const LikkleDes = styled.div``;
+export const RightSB = styled.div``;
+export const SubButton = styled.div``;
+export const FooterContainer = styled.div``;
+export const FooterBox = styled.div``;
+export const LeftArea = styled.div``;
+export const LogoContainer = styled.div``;
+export const HeartLogo = styled.img``;
+export const ActualLogo = styled.img``;
+export const RightArea = styled.div``;
+export const LinkBox = styled.div``;
+export const LinkHeader = styled.div``;
+export const SubLinkBox = styled.div``;
+export const Links = styled(Link)``;
