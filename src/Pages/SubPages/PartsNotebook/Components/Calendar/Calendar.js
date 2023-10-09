9aa1e6d46a3824f72013dd182808a8d22fdeb839
Hashcash-bit <hashirimam2004@gmail.com>
2023-10-09 17:41:34 -0400

    Update: May Done

---
+++ src/Pages/SubPages/PartsNotebook/Components/Calendar/Calendar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import May from './Components/May/May';
+
+export default function Calendar() {
+  return (
+    <div>
+      <May />
+    </div>
+  );
+}
--- src/Pages/SubPages/PartsNotebook/PartsNotebook.js
+++ src/Pages/SubPages/PartsNotebook/PartsNotebook.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Title from './Components/Title/Title';
 import Contributions from './Components/Contributions/Contributions';
 import Parts from './Components/Parts/Parts';
+import Calendar from './Components/Calendar/Calendar';
 
 export default function PartsNotebook() {
   return (
@@ -9,6 +10,7 @@ export default function PartsNotebook() {
       <Title />
       <Contributions />
       <Parts />
+      <Calendar />
     </div>
   );
 }
